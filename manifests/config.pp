a9e15598aeacf7bf87e5b4a57b5f9c71ebb5b65b
Johan Fleury <jfleury@arcaik.net>
2019-03-11 12:31:38 -0400

    json arrays

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,18 +7,7 @@ class webhook::config {
     owner          => 'root',
     group          => 'root',
     mode           => '0640',
+    format         => 'json-array-pretty',
     ensure_newline => true,
-  }
-
-  ::concat::fragment { 'webhook json-array-opening':
-    target  => '/etc/webhook.conf',
-    order   => '01',
-    content => '[',
-  }
-
-  ::concat::fragment { 'webhook json-array-closing':
-    target  => '/etc/webhook.conf',
-    order   => '99',
-    content => ']',
   }
 }
